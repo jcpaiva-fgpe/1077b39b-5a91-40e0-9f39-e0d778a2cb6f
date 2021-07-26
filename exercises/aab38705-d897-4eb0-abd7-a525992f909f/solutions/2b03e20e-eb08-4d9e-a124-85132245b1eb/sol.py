090a8838743a7bbb8b383232adf68e49d60cc0be
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-26 23:29:33 +0100

    updated file exercises/aab38705-d897-4eb0-abd7-a525992f909f/solutions/2b03e20e-eb08-4d9e-a124-85132245b1eb/sol.py

--- exercises/aab38705-d897-4eb0-abd7-a525992f909f/solutions/2b03e20e-eb08-4d9e-a124-85132245b1eb/sol.py
+++ exercises/aab38705-d897-4eb0-abd7-a525992f909f/solutions/2b03e20e-eb08-4d9e-a124-85132245b1eb/sol.py
@@ -1,3 +1,4 @@
-st = input().lower().split()
-res = {i : st.count(i) for i in set(st) if st.count(i)>1}
-print('contains' if len(res) != 0 else 'does not contain')
+st = input()
+lif = st.replace('.', '').replace(',', '').replace('(', '').replace(')', '').replace('!', '').replace('?', '').lower().split()
+res = {i : lif.count(i) for i in set(lif) if lif.count(i)>1}
+print ('contains' if len (res)!= 0 else 'does not contain')
