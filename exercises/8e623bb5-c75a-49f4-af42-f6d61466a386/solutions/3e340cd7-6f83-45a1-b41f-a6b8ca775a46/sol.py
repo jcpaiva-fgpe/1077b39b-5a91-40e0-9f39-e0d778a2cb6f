bd62b8a219ed253611eddbc0fbfde286cca722a4
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-26 21:20:30 +0100

    updated file exercises/8e623bb5-c75a-49f4-af42-f6d61466a386/solutions/3e340cd7-6f83-45a1-b41f-a6b8ca775a46/sol.py

--- exercises/8e623bb5-c75a-49f4-af42-f6d61466a386/solutions/3e340cd7-6f83-45a1-b41f-a6b8ca775a46/sol.py
+++ exercises/8e623bb5-c75a-49f4-af42-f6d61466a386/solutions/3e340cd7-6f83-45a1-b41f-a6b8ca775a46/sol.py
@@ -1,13 +1,11 @@
 d = {}
 while True:
     print('Enter the result:')
-    res = input().split()
     try:
-        if res[0] != 'end':
-            d[res[0]] = float(res[1])
-            maxv = max(d.values())
-            maxk = [k for k, v in d.items() if v == maxv] 
-            print('Leader:', *maxk, maxv)
-        else: break
+        res = str(input()).split()
+        d[res[0]] = float(res[1])
+        maxv = max(d.values())
+        maxk = [k for k, v in d.items() if v == maxv] 
+        print('Leader:', *maxk, maxv)
     except:
         break
