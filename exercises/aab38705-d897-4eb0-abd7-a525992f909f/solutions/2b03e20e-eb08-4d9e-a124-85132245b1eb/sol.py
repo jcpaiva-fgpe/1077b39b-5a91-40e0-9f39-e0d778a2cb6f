ac747aab93241c15d2d638ee8d8e4ae390681cc9
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-29 09:39:55 +0100

    updated file exercises/aab38705-d897-4eb0-abd7-a525992f909f/solutions/2b03e20e-eb08-4d9e-a124-85132245b1eb/sol.py

--- exercises/aab38705-d897-4eb0-abd7-a525992f909f/solutions/2b03e20e-eb08-4d9e-a124-85132245b1eb/sol.py
+++ exercises/aab38705-d897-4eb0-abd7-a525992f909f/solutions/2b03e20e-eb08-4d9e-a124-85132245b1eb/sol.py
@@ -1,4 +1,4 @@
 st = input()
 lif = st.replace('.', '').replace(',', '').replace('(', '').replace(')', '').replace('!', '').replace('?', '').lower().split()
 res = {i : lif.count(i) for i in set(lif) if lif.count(i)>1}
-print ('contains' if len (res)!= 0 else 'does not contain')
+print (True if len (res)!= 0 else False)
