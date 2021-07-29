a617447f55f8408665af79a9044c4b9d791ab6fc
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-29 12:21:44 +0100

    updated file exercises/cbe0f2d1-0dc3-40d1-883b-4478103f9280/solutions/f716294d-f254-43f7-91f2-ee7c8370901f/sol.py

--- exercises/cbe0f2d1-0dc3-40d1-883b-4478103f9280/solutions/f716294d-f254-43f7-91f2-ee7c8370901f/sol.py
+++ exercises/cbe0f2d1-0dc3-40d1-883b-4478103f9280/solutions/f716294d-f254-43f7-91f2-ee7c8370901f/sol.py
@@ -1,2 +1,6 @@
-temp = float(input("Kelvin\n"))
-print(f"{temp + 273.15}°C")
+while True:
+    try:
+        temp = float(input("Kelvin?\n"))
+        print(f"{round(temp + 273.15, 2)}°C")
+    except ValueError:
+        break
