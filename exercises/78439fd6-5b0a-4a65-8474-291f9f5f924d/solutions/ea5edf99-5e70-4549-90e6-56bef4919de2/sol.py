ce1d87b576016fe4b10536bc45cd4c9b848a6089
Thomas Naprawski <tnaprawski@gmail.com>
2021-07-29 09:26:01 +0100

    updated file exercises/78439fd6-5b0a-4a65-8474-291f9f5f924d/solutions/ea5edf99-5e70-4549-90e6-56bef4919de2/sol.py

--- exercises/78439fd6-5b0a-4a65-8474-291f9f5f924d/solutions/ea5edf99-5e70-4549-90e6-56bef4919de2/sol.py
+++ exercises/78439fd6-5b0a-4a65-8474-291f9f5f924d/solutions/ea5edf99-5e70-4549-90e6-56bef4919de2/sol.py
@@ -1,9 +1,9 @@
 letter = 'A'
 while True:
-    print(f"Enter a word beginning with {letter}:")
+    print(f"Word - {letter}***?")
     word = input().upper()
     if word[0] == letter:
         letter = word[-1]
      else:
-        print("You lost.")
+        print("Game over.")
         break
