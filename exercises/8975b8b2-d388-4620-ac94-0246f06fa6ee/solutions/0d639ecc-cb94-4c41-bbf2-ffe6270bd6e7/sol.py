5382c082eb738e861311f1f27566f4f832ca4a9a
Thomas Naprawski <tnaprawski@gmail.com>
2021-08-03 12:33:31 +0100

    updated file exercises/8975b8b2-d388-4620-ac94-0246f06fa6ee/solutions/0d639ecc-cb94-4c41-bbf2-ffe6270bd6e7/sol.py

--- exercises/8975b8b2-d388-4620-ac94-0246f06fa6ee/solutions/0d639ecc-cb94-4c41-bbf2-ffe6270bd6e7/sol.py
+++ exercises/8975b8b2-d388-4620-ac94-0246f06fa6ee/solutions/0d639ecc-cb94-4c41-bbf2-ffe6270bd6e7/sol.py
@@ -6,8 +6,8 @@ print('Out?')
 j = input()
 if i in przystanki and j in przystanki:
     if przystanki.index(i) < przystanki.index(j):
-        print("Przystanki to:", *przystanki[przystanki.index(i)+1:przystanki.index(j)])
+        print("Route:", *przystanki[przystanki.index(i)+1:przystanki.index(j)])
     else: 
-        print("Przystanki to:", *przystanki[przystanki.index(j)+1:przystanki.index(i)][::-1])
+        print("Route:", *przystanki[przystanki.index(j)+1:przystanki.index(i)][::-1])
 else: 
-    print("Brak takiego przystanku.")
+    print(False)
