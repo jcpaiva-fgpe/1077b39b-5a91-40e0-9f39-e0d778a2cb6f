152a10242e27f75c2868adb653a2bb0aab1bd010
Thomas Naprawski <tnaprawski@gmail.com>
2022-01-05 00:28:41 +0000

    updated file exercises/78439fd6-5b0a-4a65-8474-291f9f5f924d/solutions/ea5edf99-5e70-4549-90e6-56bef4919de2/sol.py

--- exercises/78439fd6-5b0a-4a65-8474-291f9f5f924d/solutions/ea5edf99-5e70-4549-90e6-56bef4919de2/sol.py
+++ exercises/78439fd6-5b0a-4a65-8474-291f9f5f924d/solutions/ea5edf99-5e70-4549-90e6-56bef4919de2/sol.py
@@ -2,7 +2,7 @@ letter = 'A'
 while True:
     print(f"Word - {letter}***?")
     word = input().upper()
-    if word[0] == letter:
+    if word and word[0] == letter:
         letter = word[-1]
     else:
         print("Game over.")
