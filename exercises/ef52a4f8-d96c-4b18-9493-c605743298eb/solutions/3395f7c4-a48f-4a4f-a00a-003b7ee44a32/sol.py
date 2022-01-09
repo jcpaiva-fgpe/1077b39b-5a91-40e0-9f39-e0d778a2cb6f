4c39289c49ff1d321a36b98fe75b495fcae06754
Thomas Naprawski <tnaprawski@gmail.com>
2022-01-09 09:50:56 +0000

    created file exercises/ef52a4f8-d96c-4b18-9493-c605743298eb/solutions/3395f7c4-a48f-4a4f-a00a-003b7ee44a32/sol.py

---
+++ exercises/ef52a4f8-d96c-4b18-9493-c605743298eb/solutions/3395f7c4-a48f-4a4f-a00a-003b7ee44a32/sol.py
@@ -0,0 +1,14 @@
+print('Number of children?')
+ile = int(input())
+print('Income per member?')
+income_percapita = float(input())
+plus = 200
+if income_percapita <= 500:
+    print(f'Total child benefit: {plus*(ile)}')
+elif income_percapita > 800:
+    print(f'Total child benefit: {plus*(ile-1)}')   
+else:
+    print('Handicapped?')
+    incapacitation = input().lower()
+    if incapacitation == 'yes' or income_percapita <= 800:
+        print(f'Total child benefit: {plus*(ile)}')
