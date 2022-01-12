496c5894945b0754d32778e2b600275761e34f3a
Thomas Naprawski <tnaprawski@gmail.com>
2022-01-12 00:51:17 +0000

    updated file exercises/cbe0f2d1-0dc3-40d1-883b-4478103f9280/solutions/f716294d-f254-43f7-91f2-ee7c8370901f/sol.py

--- exercises/cbe0f2d1-0dc3-40d1-883b-4478103f9280/solutions/f716294d-f254-43f7-91f2-ee7c8370901f/sol.py
+++ exercises/cbe0f2d1-0dc3-40d1-883b-4478103f9280/solutions/f716294d-f254-43f7-91f2-ee7c8370901f/sol.py
@@ -1,13 +1,17 @@
 while True:
+    options = [['1', 'Kelvin', 'Kelvin-Celsius'], ['2', 'Celsius', 'Celsius-Kelvin']]
+    print("Kelvin-Celsius [1] / Celsius-Kelvin [2]")
     try:
-        option = int(input("Kelvin-Celsius [1] / Celsius-Kelvin [2]?\n"))
-        temp = float(input())
-        if option == 1:
+        option = input().strip()
+        if option in options[0]:
+            temp = float(input(f'Enter {options[0][1]}:\n'))
             print(f"{round(temp - 273.15, 2)}°C")
-        elif option == 2:
+        elif option in options[1]:
+            temp = float(input(f'Enter {options[1][1]}:\n'))
             print(f"{round(temp + 273.15, 2)}°K")
         else:
+            print('False')
             break
-        
     except ValueError:
+        print('False')
         break
