561eb88c668ec07e02310998b9bf83ec8fde5e6f
Thomas Naprawski <tnaprawski@gmail.com>
2022-01-12 16:40:57 +0000

    created file exercises/7265ca2e-9ac1-46e7-bdea-73353c44194c/skeletons/2e118b3c-e181-4f88-abba-43510d77a2bc/skeleton.py

---
+++ exercises/7265ca2e-9ac1-46e7-bdea-73353c44194c/skeletons/2e118b3c-e181-4f88-abba-43510d77a2bc/skeleton.py
@@ -0,0 +1,16 @@
+#Your data - choice options are here
+options = [['1', 'Kelvin', 'Kelvin-Celsius', 'K'], ['2', 'Celsius', 'Celsius-Kelvin', 'C']]
+
+#some code comes here
+
+for i in range (2): print(f'{options[i][2]} [{options[i][0]}]' + ' | '*((i+1)%2), end="") 
+print()
+
+#and here...
+
+
+
+
+#Apply the choice options in your code as they are, do not change them. 
+#Please also do not copy any elements from the options list - point on them, like in the print statement above. 
+
