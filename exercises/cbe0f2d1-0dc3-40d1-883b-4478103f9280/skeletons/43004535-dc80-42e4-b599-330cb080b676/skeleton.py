77c5f5b226f4de034202d19adb634dfaca6093cb
Thomas Naprawski <tnaprawski@gmail.com>
2022-01-12 10:03:38 +0000

    updated file exercises/cbe0f2d1-0dc3-40d1-883b-4478103f9280/skeletons/43004535-dc80-42e4-b599-330cb080b676/skeleton.py

--- exercises/cbe0f2d1-0dc3-40d1-883b-4478103f9280/skeletons/43004535-dc80-42e4-b599-330cb080b676/skeleton.py
+++ exercises/cbe0f2d1-0dc3-40d1-883b-4478103f9280/skeletons/43004535-dc80-42e4-b599-330cb080b676/skeleton.py
@@ -1,8 +1,16 @@
-#Your code here
+#Your data - choice options are here
+options = [['1', 'Kelvin', 'Kelvin-Celsius', 'K'], ['2', 'Celsius', 'Celsius-Kelvin', 'C']]
 
-options = [['1', 'Kelvin', 'Kelvin-Celsius'], ['2', 'Celsius', 'Celsius-Kelvin']]
-print("Kelvin-Celsius [1] / Celsius-Kelvin [2]")
+#some code comes here
+
+for i in range (2): print(f'{options[i][2]} [{options[i][0]}]' + ' | '*((i+1)%2), end="") 
+print()
 
 #and here...
-#Please reuse the list in your code, do not copy subelements - point on them. 
+
+
+
+
+#Please reuse the option alternatives in your code. 
+#Do not copy any elements from the options list - point on them, like in the print statement above. 
 
