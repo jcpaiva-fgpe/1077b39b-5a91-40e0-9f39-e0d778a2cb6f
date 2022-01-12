3a0f0b0058ed4d7d53a78cce856cca83471957b4
Thomas Naprawski <tnaprawski@gmail.com>
2022-01-12 16:42:07 +0000

    created file exercises/7265ca2e-9ac1-46e7-bdea-73353c44194c/solutions/ac6522ef-290a-4004-9c85-e37886ad9a8f/sol.py

---
+++ exercises/7265ca2e-9ac1-46e7-bdea-73353c44194c/solutions/ac6522ef-290a-4004-9c85-e37886ad9a8f/sol.py
@@ -0,0 +1,22 @@
+while True:
+
+    options = [['1', 'Kelvin', 'Kelvin-Celsius', 'K'], ['2', 'Celsius', 'Celsius-Kelvin', 'C']]
+    for i in range (2): print(f'{options[i][2]} [{options[i][0]}]' + ' | '*((i+1)%2), end="") 
+    print()
+
+    try:
+        option = input().strip().upper()
+        if option in options[0]:
+            temp = float(input(f'Enter {options[0][1]}:\n'))
+            print(f"{round(temp - 273.15, 2)}°C")
+        elif option in options[1]:
+            temp = float(input(f'Enter {options[1][1]}:\n'))
+            print(f"{round(temp + 273.15, 2)}°K")
+        else:
+            print(False)
+            break
+
+    except ValueError:
+        print(False)
+        break
+            
