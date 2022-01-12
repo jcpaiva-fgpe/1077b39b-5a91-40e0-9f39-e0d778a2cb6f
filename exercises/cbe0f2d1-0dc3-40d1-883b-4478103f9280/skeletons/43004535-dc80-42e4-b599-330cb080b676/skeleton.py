e81ca7f46e07618da550ddd1f9c53809d186f720
Thomas Naprawski <tnaprawski@gmail.com>
2022-01-12 01:45:58 +0000

    created file exercises/cbe0f2d1-0dc3-40d1-883b-4478103f9280/skeletons/43004535-dc80-42e4-b599-330cb080b676/skeleton.py

---
+++ exercises/cbe0f2d1-0dc3-40d1-883b-4478103f9280/skeletons/43004535-dc80-42e4-b599-330cb080b676/skeleton.py
@@ -0,0 +1,8 @@
+#Your code here
+
+options = [['1', 'Kelvin', 'Kelvin-Celsius'], ['2', 'Celsius', 'Celsius-Kelvin']]
+print("Kelvin-Celsius [1] / Celsius-Kelvin [2]")
+
+#and here...
+#Please reuse the list in your code, do not copy subelements - point on them. 
+
