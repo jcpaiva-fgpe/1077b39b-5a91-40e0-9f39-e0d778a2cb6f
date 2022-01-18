80da0c85ef787cbca110c70bcdf8917137432a44
Thomas Naprawski <tnaprawski@gmail.com>
2022-01-18 21:22:20 +0000

    created file exercises/c93a26a0-c053-43e8-b841-3228ac3984e5/skeletons/5b47568c-a9d6-4bba-9e63-c0c0bb0258dd/skeleton.py

---
+++ exercises/c93a26a0-c053-43e8-b841-3228ac3984e5/skeletons/5b47568c-a9d6-4bba-9e63-c0c0bb0258dd/skeleton.py
@@ -0,0 +1,13 @@
+while True:
+    try:
+        rollers = int(input("A?\n"))
+        brushes = int(input("B?\n"))
+        
+        #Your code here
+        
+        p_id = ['S', 'L']
+
+        #Your code here
+
+    except:
+        break
