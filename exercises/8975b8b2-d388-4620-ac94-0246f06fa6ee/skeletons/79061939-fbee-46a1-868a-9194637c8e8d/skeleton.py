cec537ed77905718700cd047cb7f5aacc5c315c1
Thomas Naprawski <tnaprawski@gmail.com>
2022-01-18 21:29:41 +0000

    updated file exercises/8975b8b2-d388-4620-ac94-0246f06fa6ee/skeletons/79061939-fbee-46a1-868a-9194637c8e8d/skeleton.py

--- exercises/8975b8b2-d388-4620-ac94-0246f06fa6ee/skeletons/79061939-fbee-46a1-868a-9194637c8e8d/skeleton.py
+++ exercises/8975b8b2-d388-4620-ac94-0246f06fa6ee/skeletons/79061939-fbee-46a1-868a-9194637c8e8d/skeleton.py
@@ -1,1 +1,10 @@
 silkroad = "Beijing - Xian - Lanzhou - Xiahe - Wuwei - Zhangye - Jiayuguan Pass - Dunhuang - Turpan - Urumqi - Kashgar - Shanghai"
+
+print('In?')
+#Get user data
+print('Out?')
+#Get user data
+
+#Your code here
+
+print("Route:",) #modify the print function
