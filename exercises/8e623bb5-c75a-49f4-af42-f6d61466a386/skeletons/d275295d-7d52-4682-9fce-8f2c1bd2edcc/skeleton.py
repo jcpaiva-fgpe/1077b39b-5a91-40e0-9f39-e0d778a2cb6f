0807341b783f54a86ad95a04097ddc8ab8492bd6
Thomas Naprawski <tnaprawski@gmail.com>
2022-01-19 21:58:12 +0000

    created file exercises/8e623bb5-c75a-49f4-af42-f6d61466a386/skeletons/d275295d-7d52-4682-9fce-8f2c1bd2edcc/skeleton.py

---
+++ exercises/8e623bb5-c75a-49f4-af42-f6d61466a386/skeletons/d275295d-7d52-4682-9fce-8f2c1bd2edcc/skeleton.py
@@ -0,0 +1,11 @@
+d = {}
+while True:
+    print('Result?')
+    try:
+        
+        # Your code here
+
+        #Display the output
+        print('Leader:', ) #modify the print statement
+    except:
+        break
