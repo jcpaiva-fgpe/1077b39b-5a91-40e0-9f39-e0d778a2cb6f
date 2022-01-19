a0e6db199607fba28dd5034974e5d22f016dfcce
Thomas Naprawski <tnaprawski@gmail.com>
2022-01-19 17:47:34 +0000

    updated file exercises/ef52a4f8-d96c-4b18-9493-c605743298eb/skeletons/fdd2bee4-b497-4548-a5b3-5b1003e14b8f/skeleton.py

--- exercises/ef52a4f8-d96c-4b18-9493-c605743298eb/skeletons/fdd2bee4-b497-4548-a5b3-5b1003e14b8f/skeleton.py
+++ exercises/ef52a4f8-d96c-4b18-9493-c605743298eb/skeletons/fdd2bee4-b497-4548-a5b3-5b1003e14b8f/skeleton.py
@@ -1,9 +1,12 @@
-#defining user inputs
+# Defining user inputs
 print('Number of children?')
 #input() 
 print('Income per member?')
 #input()
 
+# Call the variable names in the output where necessary
+total = 'Total child benefit:'
+h = 'Handicapped?'
 
-#extend the program here
+# Extend the program here
 
