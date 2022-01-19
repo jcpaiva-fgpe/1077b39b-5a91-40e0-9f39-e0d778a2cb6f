e1e9760140bca059058c74686c52d1f8beb1b4c3
Thomas Naprawski <tnaprawski@gmail.com>
2022-01-19 21:12:52 +0000

    updated file exercises/1cbbdcfb-e598-41f5-826c-c1f24695ca9c/skeletons/7b8514b3-487c-4ee0-be87-dca3802491c6/skeleton.py

--- exercises/1cbbdcfb-e598-41f5-826c-c1f24695ca9c/skeletons/7b8514b3-487c-4ee0-be87-dca3802491c6/skeleton.py
+++ exercises/1cbbdcfb-e598-41f5-826c-c1f24695ca9c/skeletons/7b8514b3-487c-4ee0-be87-dca3802491c6/skeleton.py
@@ -3,3 +3,5 @@ print('String:')
 
 
 #Display the output
+#You can use the join method in the print statement to display the whitespace character between elements.
+#Note: Please avoid any other unwanted characters, exemplary an whitespace character can not occur on the end of the sentence.
