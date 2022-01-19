b3651716af360b9f8c08b425e066484b2c11798b
Thomas Naprawski <tnaprawski@gmail.com>
2022-01-19 21:17:23 +0000

    updated file exercises/787c5f67-8f75-4a64-98b8-fd9764910c9b/skeletons/1b6f83ec-ab2b-4014-b5da-e62a879d6669/skeleton.py

--- exercises/787c5f67-8f75-4a64-98b8-fd9764910c9b/skeletons/1b6f83ec-ab2b-4014-b5da-e62a879d6669/skeleton.py
+++ exercises/787c5f67-8f75-4a64-98b8-fd9764910c9b/skeletons/1b6f83ec-ab2b-4014-b5da-e62a879d6669/skeleton.py
@@ -2,4 +2,8 @@ print('Pyramids name?')
 #User input data
 
 #Your code here
-print(''.join()) # modify the print function
+
+#You can use the join method in the print statement to display the whitespace character between elements.
+print(' '.join()) # modify the print function
+
+#Note: Please avoid any other unwanted characters, exemplary an whitespace character can not occur on the end of each line.
